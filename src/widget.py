cdf91b6c141c8ee6718ab300980f79d98fa4c061
Groker <ymlikharev@list.ru>
2026-06-28 18:57:13 +0300

    Modification the mask_account_card function in widget.py

--- src/widget.py
+++ src/widget.py
@@ -4,14 +4,16 @@ from datetime import datetime
 from masks import get_mask_card_number, get_mask_account
 
 
-def mask_account_card(card: str) -> str:
+def mask_account_card(card: str | None) -> str:
     """
     Принимает строку с типом и номером карты или счета.
     Возвращает строку с замаскированным номером.
     """
+    if card.count(" ") > 2:
+        raise ValueError("Можно вводить только 1 номер счета")
     # Нашел все числа
     match = re.search(r"\d+", card)
-    # Проверка на введенный номер если
+    # Проверка на введенный номер если номер счета не передали
     if not match:
         raise ValueError("Номер карты или счета не найден")
 
@@ -27,7 +29,7 @@ def mask_account_card(card: str) -> str:
 
 
 def get_date(date: str) -> str:
-    """
+    r"""
     Функция принимает данные даты в строке iso формата
     извлекает дату месяц год и возвращает
     дату месяц год из строки которую передали
@@ -44,4 +46,6 @@ def get_date(date: str) -> str:
     return dt.strftime("%d.%m.%Y")
 
 
-print(get_date("2024-03-11T02:26:18.671407"))
+print(mask_account_card("""Maestro 1596837868705199
+Счет 64686473678894779589
+MasterCard 7158300734726758"""))
