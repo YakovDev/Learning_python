ae52ed71ddf69ce44df6e583ddc93cfda3662a70
Groker <ymlikharev@list.ru>
2026-07-08 13:55:03 +0300

    Refactoring processing

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
-from widget import get_date
+from datetime import datetime
+from typing import Any
 
 
 def filter_by_state(list_of_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
@@ -17,12 +18,22 @@ def filter_by_state(list_of_dict: list[dict], state: str = "EXECUTED") -> list[d
     return result
 
 
-def sort_by_date(list_of_dict: list[dict], procedure=False) -> list:
+def sort_by_date(list_of_dict: list[dict[str, Any]], procedure: str = "True") -> list[dict[str, Any]]:
     """
     Функция принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание).
     Функция возвращает новый список, отсортированный по дате (date).
     """
-    date = (get_date(list_of_item.get("date")) for list_of_item in list_of_dict)
-    date = list(date)
-    return sorted(date, reverse=procedure)
+    # Перевод из строки в булево значение
+    if procedure.lower().capitalize() == "True":
+        reverse = True
+    elif procedure.lower().capitalize() == "False":
+        reverse = False
+    else:  # Если передана не корректная строка
+        reverse = True
+
+    return sorted(
+        list_of_dict,
+        key=lambda x: datetime.fromisoformat(x.get("date")) if x.get("date") else datetime.min,
+        reverse=reverse,
+    )
