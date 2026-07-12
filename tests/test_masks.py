cede475c2c0f6283bec862c53eb12743c4622009
Groker <ymlikharev@list.ru>
2026-07-12 14:12:01 +0300

    add test directory and  add tests

---
+++ tests/test_masks.py
@@ -0,0 +1,26 @@
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize(
+    "input_card,expected",
+    [
+        (1234567891012131, "1234 56** **** 2131"),
+        ("1234567891012131"[::-1], "1312 10** **** 4321"),
+    ],
+)
+def test_get_mask_card_number(input_card, expected):
+    assert get_mask_card_number(input_card) == expected
+
+
+def test_get_mask_card_number_error():
+    with pytest.raises(ValueError):
+        get_mask_card_number("")
+
+
+# @pytest.mark.parametrize("input, expected ",
+#                          [
+#                              ()]
+#
+#
+# )
