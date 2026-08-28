1fb789fd8c4dd2ffe73947ff1612edee86c7469c
Groker <ymlikharev@list.ru>
2026-08-28 23:05:59 +0300

    Refactoring utils.py (change name functions:load_transactions_json) and widget.py added more stability code.

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,17 @@
 import json
 import logging
 import os
+from pathlib import Path
 
-# Определяем корень проекта (папка, где находится этот файл, поднимаемся на уровень выше)
+# Определил путь до файла с операциями по счетам
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
+JSON_PATH = DATA_DIR / "operations.json"
+
+# Определяю корень проекта (папка, где находится этот файл, поднимаемся на уровень выше)
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(current_dir)  # если файл лежит в src/, корень — на уровень выше
+
 logs_dir = os.path.join(project_root, "logs")
 os.makedirs(logs_dir, exist_ok=True)
 log_path = os.path.join(logs_dir, "utils.log")  # отдельный файл для модуля
@@ -19,7 +26,7 @@ logging.basicConfig(
 logger_load_transactions = logging.getLogger("Load_Transactions.utils")
 
 
-def load_transactions(file_path: str) -> list[dict]:
+def load_transactions_json(file_path: str) -> list[dict]:
     """
     Принимает путь до файла читает файл если файл список то возвращает его.
     Если файл пустой вернет пустой список.
--- src/widget.py
+++ src/widget.py
@@ -10,22 +10,16 @@ def mask_account_card(card: str | int) -> str:
     Возвращает строку с замаскированным номером.
     """
     card = str(card)
-    if card.count(" ") > 2:
-        raise ValueError("Можно вводить только 1 номер счета")
-    # Нашел все числа
     match = re.search(r"\d+", card)
-    # Проверка на введенный номер если номер счета не передали
     if not match:
-        raise ValueError("Номер карты или счета не найден")
+        return card
 
     numbers = match.group()
-    # Проверяю счет ли это и маскирую номер счета
     if "Счет" in card:
         masked = get_mask_account(numbers)
     else:
         masked = get_mask_card_number(numbers)
 
-    # Возвращаю новую строку с маскировкой счета
     return card.replace(numbers, masked)
 
 
@@ -35,5 +29,8 @@ def get_date(date: str) -> str:
     извлекает дату месяц год и возвращает
     дату месяц год из строки которую передали
     """
-    dt = datetime.fromisoformat(date)
-    return dt.strftime("%d.%m.%Y")
+    try:
+        dt = datetime.fromisoformat(date)
+        return dt.strftime("%d.%m.%Y")
+    except ValueError:
+        return "Дата неизвестна"
